72d44e8099e7db6ceb133ba03629e3dc41ab9068
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-05-05 15:59:03 +0200

    Adding PYTHON environment management

--- yahoo_finance/main.py
+++ yahoo_finance/main.py
@@ -1,4 +1,8 @@
+import os
 import time
+
+from dotenv import load_dotenv
+
 from yaml_reader import YamlPipelineExecutor
 
 """
@@ -9,10 +13,14 @@ Adding Queue
 
 
 def main():
+    # TODO: load environment .env file
+    load_dotenv(dotenv_path='.env_local')
+
+    print(os.environ)
 
     calc_start_time = time.time()
-
-    yaml_pipeline_executor = YamlPipelineExecutor(pipeline_location='pipelines/wiki_yahoo_scrapper_pipeline.yaml')
+    pipeline_location = os.environ.get('PIPELINE_LOCATION', 'pipelines/wiki_yahoo_scrapper_pipeline.yaml')
+    yaml_pipeline_executor = YamlPipelineExecutor(pipeline_location=pipeline_location)
     yaml_pipeline_executor.start()
 
     print("[*] Extracting Time Took: ", round(time.time() - calc_start_time, 1))
