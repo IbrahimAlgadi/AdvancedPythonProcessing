2d50381c77a546a6ba486bc96fbd49312a89157e
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-05-07 16:06:51 +0200

    We Can't Skip await

--- Asynchronous/1_async_io.py
+++ Asynchronous/1_async_io.py
@@ -6,11 +6,18 @@ async def async_sleep():
     """
     Await asyncio sleep co-routine
     """
+    print("[*] Before Sleep")
     await asyncio.sleep(5)
+    print("[*] After Sleep")
+
+
+async def print_hello():
+    print("Hello")
 
 
 async def main():
     await async_sleep()
+    await print_hello()
 
 
 if __name__ == '__main__':
